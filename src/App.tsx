9faf26ce4026eb898afdea9de947f0f8c0fd6815
Anthony Avila <anthony.p.avila@gmail.com>
2022-08-07 21:28:51 +0900

    create react app ts boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>10 Seconds: The Game</h1>
+    </div>
+  );
+}
+
+export default App;
