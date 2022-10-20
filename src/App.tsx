77762f126641b989b72d58c5e955f7f415eceef5
Anthony Avila <anthony.p.avila@gmail.com>
2022-10-20 12:59:57 +0900

    refactor some functionsin App.tsx with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
 import "./App.css";
 import { GameButton } from "./components/GameButton";
+// import readySound from "./assets/ready2count_optimal.m4a";
 
 function App() {
+  const tempVar = ["", "Ready", "Ready", "3", "2", "1", "Go!"];
+
+  const state = {
+    gameOver: false,
+    time: 0,
+    startTime: 0,
+    updatedTime: 0,
+    instructionText: [
+      "press START",
+      "press STOP after 10 seconds",
+      "game over",
+    ],
+  };
+
+  const [gameOn, setGameOn] = useState(false);
+  const [countDownText, setcountDownText] = useState(tempVar);
+  const [secondsCountingStarted, setSecondsCountingStarted] = useState(false);
+  const [startTime, setStartTime] = useState(0);
+
+  const onStartButtonClicked = () => {
+    // audioReady.play();
+    setGameOn(true);
+    handleCountdownText();
+  };
+
+  const handleCountdownText = () => {
+    const counter = countDownText;
+
+    if (counter.length > 1) {
+      setTimeout(() => {
+        counter.shift();
+        setcountDownText(counter);
+        handleCountdownText();
+      }, 1000);
+    } else {
+      clockStart();
+      setSecondsCountingStarted(true);
+    }
+  };
+
+  const tick = () => {};
+
+  const clockStart = () => {
+    // setStartTime(new Date().getTime());
+    // this.intervalID = setInterval(() => this.tick(), 10);
+  };
+
+  const clockStop = () => {};
+
+  const addTimerClasses = () => {};
+
+  const addDifferenceClasses = () => {};
+
+  const resetGame = () => {};
+
+  const setInstructions = () => {};
+
+  const setInstructionsClasses = () => {};
+
   return (
     <div className="App">
       <div className="container">
